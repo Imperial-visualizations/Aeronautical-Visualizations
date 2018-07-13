e8fa205269c6e3bb9c6c6687935ccd7a95c2934b
Nanorice <37878057+Nanorice@users.noreply.github.com>
2018-07-13 14:11:17 +0100

    stage before no. of elements is implemented

--- Hang/Rod_Vibration/L1Rod.js
+++ Hang/Rod_Vibration/L1Rod.js
@@ -1,10 +1,13 @@
-
-let x = [];
+// initialize node coordinates
+let x = [], origin = [];
 y = new Array(25).fill(0);
 for (let i = 0; i < 25; i++) {
   x[i] = i;
+  origin[i] = i;
 }
-console.log(x);
+
+let yShape = new Array(25).fill(0);
+// input mode shapes for 24-element rod
 shape = [
   [0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333,0.083333  ],
   [-0.11785,-0.11684,-0.11384,-0.10888,-0.10206,-0.0935,-0.08333,-0.07174,-0.05893,-0.0451,-0.0305,-0.01538,8.78e-16,0.015383,0.030502,0.0451,0.058926,0.071743,0.083333,0.093498,0.102062,0.10888,0.113835,0.116843,0.117851  ],
@@ -32,41 +35,74 @@ shape = [
   [0.117851,-0.11684,0.113835,-0.10888,0.102062,-0.0935,0.083333,-0.07174,0.058926,-0.0451,0.030502,-0.01538,6.98e-15,0.015383,-0.0305,0.0451,-0.05893,0.071743,-0.08333,0.093498,-0.10206,0.10888,-0.11384,0.116843,-0.11785  ],
   [-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333,0.083333,-0.08333  ]
 ];
-console.log(shape);
-let colors= [];
 
+// set color gradient
+let colors= [];
 for(i=0;i<25;i++){
-  colors.push('rgb(17,157,'+(i*10).toString()+')')
+  // colors.push('rgb('+(i*10).toString()+','+(266-(i*5)).toString()+',170)')
+  colors.push(3*i)
 }
-console.log(colors)
-var conLine = {
+// plot nodes with color gradient
+var nodePlot = {
   x: x,
   y: y,
   type: 'scatter',
   mode: 'markers',
   marker: {
     color: colors,
-    size: 3,
-    line: {
-      color: colors,
-      width: 6
-    }
-  }
+    size: 10
+  },
+  connectgaps: true
 };
-
+// graph layout
 var layout = {
-    xaxis: { },
-    yaxis: {
-      scaleanchor: "x",
-    },
-    showlegend:true
+  xaxis: {
+  range: [-5, 30],
+  showgrid: true,
+  zeroline: true,
+  showline: false,
+  autotick: true,
+  ticks: '',
+  showticklabels: false
+},
+yaxis: {
+  autorange: true,
+  showgrid: true,
+  zeroline: false,
+  showline: false,
+  autotick: true,
+  ticks: '',
+  showticklabels: false
+},
+    showlegend:false
 }
-var data = [conLine];
-Plotly.plot('graphRod', data, layout);
+var data = [nodePlot];
 
-dt = 0.0001;
-t = 0;
-n = 5;
+// adding line plots for elements with color gradient
+let xSegment = new Array(24), trace = new Array(24);
+for (let i = 0; i < 24; i++) {
+  xSegment[i] = new Array(2);
+  xSegment[i][0] = x[i];
+  xSegment[i][1] = x[i+1];
+  trace[i] = {
+    x: xSegment[i],
+    y: [0,0],
+    type: 'scatter',
+    mode: 'lines',
+    line: {
+      color: i*5,
+      width: 4
+    }
+  };
+  data.push(trace[i]);
+}
+
+
+
+
+Plotly.plot('graphRod', data, layout); // initial plot
+
+dt = 0.0001; t = 0; n = 5;
 
 $('.start.button').on('click',()=>{
   $('.start.button').addClass('active');}).on('click',() =>{
@@ -78,30 +114,92 @@ $('.pause.button').on('click',()=>{
 
 $('.start.button').on('click',begin_animation)
 
+$('.reset.button').on('click',function(){
+  for (let i = 0; i < 25; i++) {
+  x[i] = i;
+}}) // remember to add pause function to reset button !!
+
 let anim;
 
 function begin_animation(){
   function shakeAnime (){
-    let mode = $('form input.button.active').attr('href');
-    function compute (){
+    //html change bit
+    let elementNumber = $("#element").val();
+    $("#elementDisplay").html(elementNumber);
+
+    let mode = $("#modeIndex").val();
+    $("#ModeDisplay").html(mode);
+
+    let mag = $('#displacementMag').val();
+    $("#dispMag").html(mag);
+
+    let new_max = parseFloat($('input#element').val())+1;
+    $('input#modeIndex').attr('max',new_max)
+    $('#sliderMax').html(new_max)
+
+    function computeNode () {
       for (let i = 0; i < 25; i++) {
         t = t + dt;
-        /*y[i] = shape[5][i] * Math.sin(108*t) * 5;*/
-        x[i] = x[i]+shape[5][i] * Math.sin(108*t);
+        x[i] = x[i]+shape[mode-1][i] * Math.sin(108*t) * mag;
+        y[i] = shape[mode-1][i] * Math.sin(108*t);
+        // yShape[i] = shape[mode-1][i] * Math.sin(108*t);
+      }
+    };
+
+    var newNodePlot = {
+      x: origin,
+      y: y,
+      type: 'scatter',
+      mode: 'markers+lines',
+      marker: {
+        color: colors,
+        size: 10
+        // lines: {
+        //   color: 'rgb(0,0,0)'
+        // }
+      },
+      connectgaps: true
+    };
+
+    data = [newNodePlot];
+
+    function computeSegment () {
+      for (let i = 0; i < 24; i++) {
+        xSegment[i] = new Array(2);
+        xSegment[i][0] = x[i];
+        xSegment[i][1] = x[i+1];
+        trace[i] = {
+          x: xSegment[i],
+          y: [0,0],
+          type: 'scatter',
+          mode: 'lines',
+          line: {
+            color: i*5,
+            width: 4
+          }
+        };
+        data.push(trace[i]);
+      }
+    }
+
+    // yTrace = {
+    //   x: origin,
+    //   y: yTrace,
+    //   type: 'scatter',
+    //   mode: 'lines',
+    // }
+    // data.push(yTrace);
+
+    computeNode();
+    computeSegment();
+    Plotly.animate('graphRod',{data: data},
+      { transition: {duration: 0},
+        frame: {
+          duration: 10,
+          redraw: false
         }
       }
-    compute();
-    Plotly.animate('graphRod',{
-      data: [{x: x, y: y}]
-    }, {
-      transition: {
-        duration: 0
-      },
-      frame: {
-        duration: 10,
-        redraw: false
-      }
-    });
+    );
     return;
   }
 
@@ -113,3 +211,19 @@ function begin_animation(){
         $(this).text("Start")
     };
 }
+
+$(document).ready(function(){
+  $("#dispMag").hide();
+});
+
+$(document).ready(function(){
+  $(".show.button").click(function(){
+    $("#modePlot").toggle();
+  });
+});
+
+$("#elementDisplay").hover(function(){
+  alert("select the number of element you want for the rod analysis")
+},
+function(){
+});
