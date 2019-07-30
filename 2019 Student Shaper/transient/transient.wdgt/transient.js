7afd243f84762166d627f1c45b5cc1ad489367bb
Chong You Lien <lienchongyou@gmail.com>
2019-07-30 11:40:09 +0100

    updated transient interaction

--- 2019 Student Shaper/transient/transient.wdgt/transient.js
+++ 2019 Student Shaper/transient/transient.wdgt/transient.js
@@ -6,14 +6,7 @@ math.config({
 // convenience
 var graphstart = 0, graphend = 20, step = 0.02;
 var linrange = math.range(graphstart, graphend, step, true);
-let plot0, data0, layout0, plot1, data1, layout1;
-
-var temprange = math.range(0,20,1,true).toArray();
-var colorx = temprange.reduce(function(arr, v, i) {
-  return arr.concat(v, temprange[i]);
-}, []);
-var colory1 = math.zeros(colorx.length).toArray();
-var colory0 = math.zeros(linrange._size[0]).toArray();
+let data0, layout0, data1, layout1;
 
 layout0 = {
     autosize: true,
@@ -50,6 +43,35 @@ layout0 = {
     },
     showlegend: false,
     hovermode: true,
+    annotations: [{
+      x: .75,
+      y: 30,
+      xref: 'x',
+      yref: 'y',
+      text: '$r(t)$',
+      font: {
+        size: 100,
+      },
+      showarrow: true,
+      arrowhead: 1,
+      arrowcolor: "transparent",
+      ax: 0,
+      ay: 0,
+    }, {
+      x: 20,
+      y: -1,
+      xref: 'x',
+      yref: 'y',
+      text: '$t$',
+      font: {
+        size: 100,
+      },
+      showarrow: true,
+      arrowhead: 1,
+      arrowcolor: "transparent",
+      ax: 0,
+      ay: 0,
+    }, ],
   };
 
   layout1 = {
@@ -59,14 +81,14 @@ layout0 = {
       plot_bgcolor:"#F4F4F4",
       paper_bgcolor:"#F4F4F4",
       margin: {
-        l: 25,
-        r: 25,
-        b: 25,
-        t: 25,
+        l: 20,
+        r: 20,
+        b: 20,
+        t: 20,
         pad: 4
       },
       xaxis: {
-        range: [-0,20.1],
+        range: [-0,20.5],
         fixedrange: true,
         showgrid: true,
         zeroline: true,
@@ -87,6 +109,35 @@ layout0 = {
       },
       showlegend: false,
       hovermode: true,
+      annotations: [{
+        x: 2,
+        y: 20,
+        xref: 'x',
+        yref: 'y',
+        text: '$R(t)$',
+        font: {
+          size: 100,
+        },
+        showarrow: true,
+        arrowhead: 1,
+        arrowcolor: "transparent",
+        ax: 0,
+        ay: 0,
+      }, {
+        x: 20.5,
+        y: 1.5,
+        xref: 'x',
+        yref: 'y',
+        text: '$t$',
+        font: {
+          size: 100,
+        },
+        showarrow: true,
+        arrowhead: 1,
+        arrowcolor: "transparent",
+        ax: 0,
+        ay: 0,
+      }, ],
     };
 
 // initial variables
@@ -130,53 +181,11 @@ function findr (input, start, end) {
   return out;
 };
 
-function plotClick (dataInput) {
-  var newData1 = Array.from(data1);
-  var newData0 = Array.from(data0);
-  var i = math.floor(dataInput.points[0].y);
-  if (i>=20) {return;};
-  if (colory1[2*i+2]!=0) {
-    colory1[2*i+1] = 0;
-    colory1[2*i+2] = 0;
-    colory0 = math.subtract(colory0, findr(linrange, i, i+1));
-  } else {
-    colory1[2*i+1] = i+.5;
-    colory1[2*i+2] = i+.5;
-    colory0 = math.add(colory0,findr(linrange, i, i+1));
-  };
-  newData1.unshift({
-    x: colorx,
-    y: colory1,
-    fill: 'tozeroy',
-    type: 'scatter',
-    mode: 'none',
-    hoverinfo: 'none',
-    connectgaps: false,
-  });
-  newData0.unshift({
-    x: linrange.toArray(),
-    y: colory0.toArray(),
-    fill: 'tozeroy',
-    type: 'scatter',
-    mode: 'none',
-    hoverinfo: 'none',
-    connectgaps: false,
-  });
-
-  Plotly.react(plot1, data1, layout1); // refresh graph
-  Plotly.react(plot1, newData1, layout1);
-
-  Plotly.react(plot0, data0, layout0);
-  Plotly.react(plot0, newData0, layout0);
-
-  return;
-};
-
 $(document).ready(function () {
   findConsts();
   totaly = findr(linrange, graphstart, graphend);
 
-  plot0 = document.getElementById('graph0');
+  var plot0 = document.getElementById('graph0');
   data0 = [{ // first curve
       x: linrange.toArray(),
       y: totaly.toArray(),
@@ -184,18 +193,33 @@ $(document).ready(function () {
       mode: 'markers+lines',
       hoverinfo: 'none',
       marker: {
-        color: 'black',
+        color: 'transparent',
         size: 1,
       },
       line: {
-        color: 'black',
+        color: 'grey',
         width: 3,
     },
       connectgaps: false,
   }, ];
-  Plotly.newPlot(plot0, data0, layout0, {displayModeBar: false, doubleClick: false,})
+  var tempData0 = Array.from(data0);
+  var tempColor = math.zeros(linrange._size[0]);
+  tempData0.push({
+    x: linrange.toArray(),
+    y: tempColor.toArray(),
+    type: 'scatter',
+    mode: 'lines',
+    line: {
+      color: "blue",
+      width: 3,
+      dash: "dot",
+    },
+    hoverinfo: 'none',
+    connectgaps: false,
+  });
+  Plotly.newPlot(plot0, tempData0, layout0, {displayModeBar: false, doubleClick: false,});
 
-  plot1 = document.getElementById('graph1');
+  var plot1 = document.getElementById('graph1');
   data1 = [{ // horizontal
       x: linrange.toArray(),
       y: linrange.toArray(),
@@ -213,10 +237,58 @@ $(document).ready(function () {
       connectgaps: false,
   }, ];
 
-  Plotly.newPlot(plot1, data1, layout1, {displayModeBar: false, doubleClick: false,})
+  Plotly.newPlot(plot1, data1, layout1, {displayModeBar: false, doubleClick: false,});
+
+  var temprange = math.range(0,20,1,true).toArray();
+  var colorx = temprange.reduce(function(arr, v, i) {
+    return arr.concat(v, temprange[i]);
+  }, []);
+  var colory1 = math.zeros(colorx.length).toArray();
+  var colory0 = math.zeros(linrange._size[0]);
 
   plot1.on('plotly_click', function (dataInput) {
-    plotClick(dataInput);
+    var newData1 = Array.from(data1);
+    var newData0 = Array.from(data0);
+    var i = math.floor(dataInput.points[0].y);
+    if (i>=20) {return;};
+    if (colory1[2*i+2]!=0) {
+      colory1[2*i+1] = 0;
+      colory1[2*i+2] = 0;
+      colory0 = math.subtract(colory0, findr(linrange, i, i+1));
+    } else {
+      colory1[2*i+1] = i+.5;
+      colory1[2*i+2] = i+.5;
+      colory0 = math.add(colory0,findr(linrange, i, i+1));
+    };
+    newData1.unshift({
+      x: colorx,
+      y: colory1,
+      fill: 'tozeroy',
+      type: 'scatter',
+      mode: 'none',
+      hoverinfo: 'none',
+      connectgaps: false,
+    });
+    newData0.push({
+      x: linrange.toArray(),
+      y: colory0.toArray(),
+      type: 'scatter',
+      mode: 'lines',
+      line: {
+        color: 'blue',
+        width: 3,
+        dash: "dot",
+      },
+      hoverinfo: 'none',
+      connectgaps: false,
+    });
+
+    Plotly.react(plot1, data1, layout1); // refresh graph
+    Plotly.react(plot1, newData1, layout1);
+
+    Plotly.react(plot0, data0, layout0);
+    Plotly.react(plot0, newData0, layout0);
+
     return;
   });
 
@@ -241,12 +313,16 @@ $(document).ready(function () {
           hoverinfo: 'none',
           connectgaps: false,
         });
-        newData0.unshift({
+        newData0.push({
           x: linrange.toArray(),
           y: colory0.toArray(),
-          fill: 'tozeroy',
           type: 'scatter',
-          mode: 'none',
+          mode: 'lines',
+          line: {
+            color: "blue",
+            width: 3,
+            dash: "dot",
+          },
           hoverinfo: 'none',
           connectgaps: false,
         });
@@ -259,10 +335,24 @@ $(document).ready(function () {
         break;
       case "button0":
       default:
-        colory0 = math.zeros(linrange._size[0]).toArray();
+        colory0 = math.zeros(linrange._size[0]);
         colory1 = math.zeros(colorx.length).toArray();
+        var newData0 = Array.from(data0);
+        newData0.push({
+          x: linrange.toArray(),
+          y: colory0.toArray(),
+          type: 'scatter',
+          mode: 'lines',
+          line: {
+            color: "blue",
+            width: 3,
+            dash: "dot",
+          },
+          hoverinfo: 'none',
+          connectgaps: false,
+        });
         Plotly.react(plot1, data1, layout1);
-        Plotly.react(plot0, data0, layout0);
+        Plotly.react(plot0, newData0, layout0);
         break;
     };
     return;
@@ -282,21 +372,52 @@ $(document).ready(function () {
         mode: 'markers+lines',
         hoverinfo: 'none',
         marker: {
-          color: 'black',
+          color: 'transparent',
           size: 1,
         },
         line: {
-          color: 'black',
+          color: 'grey',
           width: 3,
       },
         connectgaps: false,
     }, ];
 
-    colory0 = math.zeros(linrange._size[0]).toArray();
-    colory1 = math.zeros(colorx.length).toArray();
+    colory0 = math.zeros(linrange._size[0]);
 
-    Plotly.react(plot1, data1, layout1);
-    Plotly.react(plot0, data0, layout0);
+    for (var i = 0; i < (colorx.length/2 - 1); i++) {
+      if (colory1[2*i+1]!=0) {
+        colory0 = math.add(colory0, findr(linrange, i, i+1));
+      };
+    }
+
+    var newData0 = Array.from(data0);
+    newData0.push({
+      x: linrange.toArray(),
+      y: colory0.toArray(),
+      type: 'scatter',
+      mode: 'lines',
+      line: {
+        color: "blue",
+        width: 3,
+        dash: "dot",
+      },
+      hoverinfo: 'none',
+      connectgaps: false,
+    });
+
+    var newData1 = Array.from(data1);
+    newData1.unshift({
+      x: colorx,
+      y: colory1,
+      fill: 'tozeroy',
+      type: 'scatter',
+      mode: 'none',
+      hoverinfo: 'none',
+      connectgaps: false,
+    });
+
+    Plotly.react(plot1, newData1, layout1);
+    Plotly.react(plot0, newData0, layout0);
     return;
   })
   return;
